454ab975f6b1dddcf6b723b3a28e2f556b04590f
Romain <thiriot2002@gmail.com>
2024-02-01 12:18:46 +0100

    debut
    tuile + html + suivi iss

--- assets/map.js
+++ assets/map.js
@@ -1,10 +1,48 @@
-
-
-var map = L.map('map').setView([0,0], 3);
+// Insertion de la tuile //
+let map = L.map('map').setView([0, 0], 3);
 L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	subdomains: 'abcd',
-	maxZoom: 15
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    subdomains: 'abcd',
+    maxZoom: 15
 }).addTo(map);
 
+// Création du JSON et initialisation de l'url vers l'API ISS
+let geo = L.geoJSON().addTo(map);
+let url_api = 'http://api.open-notify.org/iss-now.json';
 
+Vue.createApp({
+    // Initialisation des variables de longitude et latitude
+    data() {
+        return {
+            long: 0,
+            lat: 0
+        }
+    },
+    methods: {
+    // Méthode récupérant les longitude et latitude depuis l'API 
+
+        pointISS() {
+            let button_affiche = document.getElementById('localise');
+            button_affiche.style.visibility = 'hidden';
+            setInterval(() =>
+            fetch(url_api)
+                .then(response => response.json())
+                .then(jason => {
+                    let long_iss = jason.iss_position.longitude;
+                    let lat_iss = jason.iss_position.latitude;
+                    this.long = long_iss;
+                    this.lat = lat_iss;
+
+                    geo.clearLayers();
+                    console.log("longitude : ", this.long ," latitude : ", this.lat, ".");
+        // Insertion du marqueur  
+                    L.marker([this.lat, this.long]).addTo(geo)
+                        .bindPopup('<img id="pesquet" src="assets/pesquet.jpeg"/><center><strong>Je suis la</strong></center>')
+                        .openPopup();
+
+                   
+                })
+                , 4000);
+        }
+    }
+}).mount("#trouver");
--- views/accueil.php
+++ views/accueil.php
@@ -5,10 +5,30 @@
      <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
      integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
      crossorigin=""></script>
-<body>
-    <h1>Tweete comme pesquet</h1>
-    <div id="map">
-    <div><script src='assets/map.js'></script></div>
-    </div>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+     <body>
+
+    <div id="trouver">
     
+    <h1><center>Tweete comme pesquet</center></h1>
+    
+      <div id="suivi">
+        <fieldset>
+        <center> <legend>Suivi de l'ISS</legend>
+        <input type="checkbox" id="suiviISS" value="SuiviISS" />
+        <label for="suiviISS">Zoomer sur l'ISS</label></center>
+       
+        </fieldset>
+      </div>
+ 
+    <button type="button" class="btn btn-primary" id="localise" @Click="pointISS">Afficher Thomas Pesquet </button>
+    </div>
+    <div id="map"></div>
+     
+   
+
+   
+      <script src="https://cdn.jsdelivr.net/npm/vue"></script>
+      <script src="assets/map.js"></script>
 </body>
