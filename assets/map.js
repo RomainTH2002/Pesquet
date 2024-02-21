da01402f46e517545b1be183f2043ebe466b03b1
Romain <thiriot2002@gmail.com>
2024-02-21 21:34:11 +0100

    retour_tweet_marche_askip
    tout

--- assets/map.js
+++ assets/map.js
@@ -17,25 +17,22 @@ let url_api = 'http://api.open-notify.org/iss-now.json';
 let url_orbit = 'https://dev.iamvdo.me/orbit.php';
 let orbite_parcourue = [];
 let orbite_future = [];
+
 Vue.createApp({
     // Initialisation des variables de longitude et latitude
     data() {
         return {
             long: 0,
             lat: 0,
-            message : 'Je suis la',
-            
+            message : 'Je suis là !',
+            marker: null // Ajoutez cette propriété pour stocker la référence au marqueur
         }
     },
     methods: {
-    // Méthode récupérant les longitude et latitude depuis l'API 
-
+        // Méthode récupérant les longitude et latitude depuis l'API 
         pointISS() {
-
             let button_affiche = document.getElementById('localise');
-            button_affiche.style.visibility = 'hidden';
-            
-                
+            button_affiche.style.visibility = 'hidden'; 
        
             setInterval(() =>
             fetch(url_api)
@@ -50,30 +47,70 @@ Vue.createApp({
                     orbite_parcourue.push(coordonnees);
                     geo.clearLayers();
                     // focus sur l'ISS
-                 //map.setView([this.long-500,this.lat], 3);
+                    //map.setView([this.long-500,this.lat], 3);
                     fetch(url_orbit)
                     .then(response => response.json())
                     .then(jason => {
                         jason.forEach(point_orb => {
-
                             orbite_future.push([point_orb.lat, point_orb.lng]);
-                        });
-                        
-                           
+                        });  
                             
-                        })
-        // Insertion du marqueur  
-                    
-                    L.marker([this.lat, this.long] , {icon: satIcone}).addTo(geo)
-                        .bindPopup('<center><img id="pesquet" src="assets/pesquet.jpeg"/></center><center><strong>'+ this.message +'</strong></center>')
+                    })
+                    // Insertion du marqueur  
+                    this.marker = L.marker([this.lat, this.long] , {icon: satIcone}).addTo(geo)
+                        .bindPopup('<center><img id="pesquet" src="assets/pesquet.jpeg"/></center>')
                         .openPopup();
                     L.polyline(orbite_future, {color: 'red',width: 4}).addTo(geo);
                     L.polyline(orbite_parcourue, {color: 'green'}).addTo(geo);
                     orbite_future = []
-                   
                 })
         // Intervalle de 4 secondes
                 ,4000);
-    }
+        },
+
+        // Méthode d'affichage de la photo en tweet
+        submitTweet(event) {
+            // Empêcher l'envoi par défaut du formulaire
+            event.preventDefault();
+            console.log('réponse');
+            // Récupérer l'orthophoto de la zone en utilisant les coordonnées de l'ISS et le niveau de zoom sélectionné
+            const zoomLevel = this.zoomLevel;
+            const latitude = this.lat;
+            const longitude = this.long;
+            const orthophotoUrl = `https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/${zoomLevel}/${latitude}/${longitude}`;
+        
+            // Générer l'URL pour l'appel à l'API Geonames
+            const username = 'eric'; // Mdr je vole le compte d'un random
+            const geonamesUrl = `http://api.geonames.org/findNearbyPlaceNameJSON?lat=${latitude}&lng=${longitude}&username=${username}`;
+        
+            // Faire un appel AJAX vers l'API Geonames pour obtenir les informations sur le lieu
+            fetch(geonamesUrl)
+                .then(response => response.json())
+                .then(data => {
+                    // Traiter les résultats pour afficher un message du type "Hello Paris #France" dans la page
+                    if (data.geonames && data.geonames.length > 0) {
+                        console.log('réponse_2');
+                        const placeName = data.geonames[0].toponymName || data.geonames[0].name;
+                        const countryName = data.geonames[0].countryName;
+                        const msg = `Hello ${placeName} #${countryName}`;
+                        
+                        if (this.marker) { // Utilisez this.marker pour accéder à l'instance de marqueur
+                            console.log('réponse_3');
+                            this.marker.setPopupContent(`<center><img id="photo" src="${orthophotoUrl}"/></center><center><strong>${msg}</strong></center>`)
+                                        .openPopup();
+                        }
+
+                    }
+                })
+                .catch(error => {
+                    console.error('Error fetching geonames data:', error);
+                    alert('An error occurred while fetching geonames data');
+                });
+        },
+        mounted() {
+            // Sélectionner le bouton et lui attacher un écouteur d'événement
+            const button = document.getElementById('envoietweet');
+            button.addEventListener('click', this.submitTweet);
+        }
     }
 }).mount("#trouver");
--- views/accueil.php
+++ views/accueil.php
@@ -1,40 +1,47 @@
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-     crossorigin=""/>
-     <link rel="stylesheet" href="assets/map.css">
-     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-     crossorigin=""></script>
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-     <body>
-
-    <div id="trouver">
-    
-   <center><h1>Tweete comme pesquet</h1></center>
-    
-      <div id="suivi">
-        <fieldset>
-        <center> <legend>Suivi de l'ISS</legend>
-        <input type="checkbox" id="suiviISS" value="SuiviISS" />
-        <label for="suiviISS">&nbsp Zoomer sur l'ISS</label>
-        <h6> longitude :  {{this.long}} | latitude {{this.lat}}</h6></center>
-        </fieldset>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tweete comme Pesquet</title>
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+    integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+    crossorigin=""/>
+  <link rel="stylesheet" href="assets/map.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+  <div id="trouver">
+    <center><h1>Tweete comme Pesquet</h1></center>
+    <div id="suivi">
+      <fieldset>
+        <center>
+          <legend>Suivi de l'ISS</legend>
+          <input type="checkbox" id="suiviISS" value="SuiviISS" />
+          <label for="suiviISS">&nbsp Zoomer sur l'ISS</label>
+          <h6> longitude :  {{this.long}} | latitude {{this.lat}}</h6>
+        </center>
+      </fieldset>
       
-        <div id="zonetweet">
-          <center><legend>Zone de Tweet</legend></center>
-        <textarea id="textweet" v-model="message" ></textarea>
-        <button id="envoietweet" class="btn btn-success" >Envoyer</button>
+      <div id="zonetweet">
+        <center><legend>Zone de Tweet</legend></center>
+        <form @submit.prevent="submitTweet">
+          <label for="zoomSmartphone"><input type="radio" id="zoomSmartphone" name="zoomLevel" value="7" v-model="zoomLevel"> Smartphone</label>
+          <label for="zoomReflex"><input type="radio" id="zoomReflex" name="zoomLevel" value="10" v-model="zoomLevel"> Réflex</label>
+          <label for="zoomTeleobjectif"><input type="radio" id="zoomTeleobjectif" name="zoomLevel" value="13" v-model="zoomLevel"> Téléobjectif</label>
+          <textarea id="textweet" v-model="message" ></textarea>
+          <button id="envoietweet" class="btn btn-success" >Tweete comme Pesquet</button>
+        </form>
       </div>
-      </div>
- 
-    <button type="button" class="btn btn-primary" id="localise" @Click="pointISS">Afficher Thomas Pesquet </button>
     </div>
-    <center><div id="map"></div></center>
-     
-   
-
-   
-      <script src="https://cdn.jsdelivr.net/npm/vue"></script>
-      <script src="assets/map.js"></script>
+ 
+    <button type="button" class="btn btn-primary" id="localise" @click="pointISS">Afficher Thomas Pesquet </button>
+  </div>
+  <center><div id="map"></div></center>
+  <script src="https://unpkg.com/vue@3.2.21/dist/vue.global.prod.js"></script>
+  <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+    integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+    crossorigin=""></script>
+  <script src="assets/map.js"></script>
 </body>
+</html>
